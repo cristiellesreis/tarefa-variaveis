7247c0f299964da6df90fa327f01d7132c8222cc
Cristielle Reis <58055010+cristiellesreis@users.noreply.github.com>
2023-06-06 22:14:11 -0300

    subindo arquivos

---
+++ arquivo.js
@@ -0,0 +1,13 @@
+var pagina = "A página foi carregada com sucesso!";
+alert(pagina);
+
+var minhaIdade=29;
+var suaIdade=30;
+
+document.write("A soma das nossas idades é: " +(minhaIdade+suaIdade));
+document.write("<br>");
+document.write("Minha idade dividida por 2 é: " +(minhaIdade/2));
+document.write("<br>");
+document.write("Sua idade multiplicada por 8 é: " +(suaIdade*8));
+document.write("<br>");
+document.write("Nossas idades são iguais? " +(minhaIdade==suaIdade));
